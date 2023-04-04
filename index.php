d75b0dd8cfa471c8f3aefa8274ded38b1fcfafb3
Adel Benyahia <a.benyahia6993@gmail.com>
2023-04-04 14:58:40 +0200

    ajout collones prix et description

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php 
-$BeaniesTab =["Bonnet en laine", "Bonnet en laine bio","Bonnet en laine et cachemire", "Bonnet arc-en-ciel"]
+$BeaniesTab =[
+    ['name'=>"Bonnet en laine",
+    'prix'=> 10, 
+    'description'=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptates labore aut odio aliquid tempore consequatur, nihil doloremque magni,"], 
+
+    ['name'=>"Bonnet en laine bio",
+    'prix'=> 14, 
+    'description'=>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio et repellat odit harum sed perspiciatis"],
+
+    ['name'=>"Bonnet en laine et cachemire",
+    'prix'=> 20, 
+    'description'=>"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia magnam adipisci temporibus maiores quo id distinctio alias earum voluptatem vitae,"], 
+
+    ['name'=>"Bonnet arc-en-ciel",
+    'prix'=> 12, 
+    'description'=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nam reprehenderit veniam aperiam impedit iure delectus,"]]
 ?>
 
 <!DOCTYPE html>
@@ -14,13 +29,16 @@ $BeaniesTab =["Bonnet en laine", "Bonnet en laine bio","Bonnet en laine et cache
 <body>
     <h1>Listes bonnets</h1>
     <table>
-        <tr><th>nom</th></tr>
+        <tr><th>nom</th><th>prix</th><th>description</th></tr>
 
     <?php 
     foreach($BeaniesTab as $beanie){
         ?>
-        <tr><td><?php echo $beanie ?> </td></tr>
-        
+        <tr>
+            <td><?php echo $beanie['name'] ?> </td>
+            <td><?php echo $beanie['prix']."€" ?> </td>
+            <td><?php echo $beanie['description'] ?> </td>
+        </tr>    
         <?php
     }
     ?>
